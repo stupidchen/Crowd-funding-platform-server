15a2b3617f130a2d7b4ea4a99633dee0f2c101e0
Mike Chen <stupidchen@foxmail.com>
2016-07-18 12:01:39 +0800

    Fixed url

--- util/uselessUtil.js
+++ util/uselessUtil.js
@@ -4,7 +4,7 @@
 'use strict';
 
 var uselessUtil = {
-    urlPool: [],
+    urlPool: ["http://img30.360buyimg.com/cf/jfs/t2794/55/3030040013/107450/5534cc7b/577dea8eN59d5e6f9.jpg","http://img30.360buyimg.com/cf/jfs/t2869/236/3123242555/310567/7fb01ea0/577e3632Na8d26f92.jpg","http://img30.360buyimg.com/cf/jfs/t2641/298/3045693626/112594/30cc6e43/577e167cN7b3def52.gif","http://cs.zhongchoujia.com/UploadFiles/Goods/2016-07/logo/thb20160701180654916416.jpg"],
     
     getUrl: function () {
         return this.urlPool;
